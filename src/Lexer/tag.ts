84d97f1cd8b066a6da6dc7cc50054e26cfe36a32
wlwywlqk <wlwywlqk@gmail.com>
2021-07-06 16:54:49 +0800

    Add Lexer

---
+++ src/Lexer/tag.ts
@@ -0,0 +1,26 @@
+export enum Tag {
+    AND,
+    OR,
+
+    IF,
+    ELSE,
+    ELSEIF,
+
+    DO,
+    WHILE,
+
+    EQ,
+    GT,
+    LT,
+    GE,
+    LE,
+    
+    ID,
+    NUM,
+    FALSE,
+    TRUE,
+
+
+    ADD,
+    MINUS,
+}
