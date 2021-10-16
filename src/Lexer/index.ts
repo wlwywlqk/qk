e415ec9aef27292e0915b87eeffb9f9d8a9ba535
wlwywlqk <wlwywlqk@gmail.com>
2021-10-16 11:02:13 +0800

    Fix Lexer catch type

--- src/Lexer/index.ts
+++ src/Lexer/index.ts
@@ -64,7 +64,7 @@ export class Lexer {
         try {
             return this.scanImpl();
         } catch (e) {
-            this.error = e;
+            this.error = e as LexerError;
             throw e;
         }
     }
